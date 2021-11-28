f8ef601d95c0729f702a4772cb84576a0943bfcf
Dino Morelli <dino@ui3.info>
2021-11-28 15:00:06 -0500

    Merged recent changes from scripts/epochconv.hs
    This project is going to be the authoritative version of epochconv going
    forward.

--- src/epochconv.hs
+++ src/epochconv.hs
@@ -1,35 +1,41 @@
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+#! /usr/bin/env runhaskell
+
+{- # OPTIONS_GHC -fno-warn-type-defaults #-}
 
 import Control.Monad ( mplus )
-import Data.List ( intercalate )
 import Data.Time
   ( FormatTime, ParseTime, UTCTime
-  , defaultTimeLocale, formatTime, getCurrentTime, parseTimeM
-  , utcToLocalZonedTime
+  , formatTime, getCurrentTime, parseTimeM, utcToLocalZonedTime
   )
+import Data.Time.Format ( defaultTimeLocale )
 import Data.Time.Clock.POSIX
   ( posixSecondsToUTCTime, utcTimeToPOSIXSeconds )
-import Data.Version ( showVersion )
-import Paths_epochconv ( version )
 import System.Environment ( getArgs, getProgName )
 import System.Exit ( exitFailure, exitSuccess )
 import Text.Printf ( printf )
 
 
-{-
-  Some important points:
+{- Some important points:
 
-  POSIXTime is basically this: secs-since-epoch.picoseconds
-  as a fractional number
+   POSIXTime is basically this: secs-since-epoch.picoseconds
+   as a fractional number
 
-  By using (fromIntegral . truncate) on it, you can strip off the picos
-  getting what we all know as epoch time. Or you can use parseTimeM True "%s"
+   By using round on it, you can strip off the picos getting what
+   we all know as epoch time. Type annotation (Int or Integer)
+   may be required. Like this:
 
-  But UTCTime is the low-level format that everything uses in the
-  Haskell APIs
+      epoch = round . utcTimeToPOSIXSeconds $ someUTCTime
 
-  Functions for converting between the two are in
-  Data.Time.Clock.POSIX
+   If you just need a String you can use parseTimeM with "%s"
+   for format string:
+
+      epochAsString = formatTime defaultTimeLocale "%s" someUTCTime
+
+   But UTCTime is the low-level format that everything uses in the
+   Haskell APIs
+
+   Functions for converting between the two are in
+   Data.Time.Clock.POSIX
 -}
 
 
@@ -41,47 +47,49 @@ parseInput :: [String] -> IO (Either String UTCTime)
 parseInput []         = Right  <$> getCurrentTime
 parseInput ["-h"]     = Left   <$> usage
 parseInput ["--help"] = Left   <$> usage
-parseInput as         = return .   parseInput' $ as
+parseInput as         = pure . parseInput' $ as
 
 
 usage :: IO String
 usage = do
-  appName <- getProgName
-  now <- getCurrentTime
-  return $ unlines $
-    [ "Show a given date (or the current date) in a variety of formats"
-    , ""
-    , "Usage: " ++ appName ++ " [OPTION] [DATE]"
-    , ""
-    , "Options:"
-    , "  -e          Input is epoch (seconds since 1970-01-01)"
-    , "  -m          Input is milliseconds (since 1970-01-01)"
-    , "  -f          Input is a human-readable date"
-    , "  -h, --help  This usage information"
-    , ""
-    , "If no date is given, the current date/time will be used."
-    , "The -e and -f options are mostly unnecessary. -m is needed to"
-    , "disambiguate between epoch and milliseconds."
-    , ""
-    , "Parsable input formats for -f:"
-    ]
-    ++ (map (\fp -> "   " ++ fmt fp now) formatPatterns) ++
-    [ ""
-    , "Output will be the date/time in a variety of formats, both localized"
-    , "and UTC, as well as epoch and milliseconds."
-    , ""
-    , "Version " ++ (showVersion version) ++ "  Dino Morelli <dino@ui3.info>"
-    ]
+   appName <- getProgName
+   now <- getCurrentTime
+   pure $ unlines $
+      [ "Show a given date (or the current date) in a variety of formats"
+      , ""
+      , "Usage: " <> appName <> " [OPTION] [DATE]"
+      , ""
+      , "Options:"
+      , "  -e          Input is epoch (seconds since 1970-01-01)"
+      , "  -m          Input is milliseconds (since 1970-01-01)"
+      , "  -f          Input is a human-readable date"
+      , "  -h, --help  This usage information"
+      , ""
+      , "If no date is given, the current date/time will be used."
+      , "The -e and -f options are mostly unnecessary. -m is needed to"
+      , "determine whether a long number is epoch or milliseconds."
+      , ""
+      , "Parsable input formats for -f:"
+      ]
+      <> map (\fp -> "  " <> fmt fp now) formatPatterns <>
+      [ ""
+      , "Output will be the date/time in a variety of formats, both localized"
+      , "and UTC, as well as epoch and milliseconds."
+      , ""
+      , "Version 1.x  Dino Morelli <dino@ui3.info>"
+      ]
 
 
 parseInput' :: [String] -> Either String UTCTime
-parseInput' ("-e" : epochString : []) = strToUTCTime 1 epochString
-parseInput' ("-m" : milliString : []) = strToUTCTime 1000 milliString
-parseInput' ("-f" : as)               = parseDateString $ joinArgs as
+parseInput' ["-e", epochString] = strToUTCTime 1 epochString
+parseInput' ["-m", milliString] = strToUTCTime 1000 milliString
+parseInput' ("-f" : as)         = parseDateString . unwords $ as
 parseInput' as
-  | any (not . (flip elem) "-0123456789")
-    $ joinArgs as = parseInput' $ "-f" : as
-  | otherwise = strToUTCTime 1 (joinArgs as)
+  -- All of the characters are not digits, probably a formatted date
+  -- The tail is here to allow a negative sign for epochs prior to 0
+  | not . all (flip elem "0123456789") . tail . unwords $ as
+    = parseInput' $ "-f" : as
+  | otherwise = strToUTCTime 1 . unwords $ as
 
 
 strToUTCTime :: Double -> String -> Either String UTCTime
@@ -89,13 +97,9 @@ strToUTCTime divisor = Right . posixSecondsToUTCTime . realToFrac
   . (/ divisor) . read
 
 
-joinArgs :: [String] -> String
-joinArgs = intercalate " "
-
-
 parseDateString :: String -> Either String UTCTime
 parseDateString s = maybe (Left $ printf "Unable to parse \"%s\"" s)
-  (Right . id) $ foldl mplus Nothing $ map ($ s) parsers
+  Right $ foldl mplus Nothing $ map ($ s) parsers
 
 
 rfc5322Date, iso1601Offset, iso1601Zulu :: String
@@ -131,14 +135,14 @@ output (Left errMsg) = do
 output (Right ut) = do
   local <- utcToLocalZonedTime ut
 
-  putStrLn $ "     RFC5322: " ++ fmt rfc5322Date local
+  putStrLn $ "     RFC5322: " <> fmt rfc5322Date local
 
   printf "\n %s ISO1601: %s\n" (fmt "%Z" local) (fmt iso1601Offset local)
-  putStrLn $ " UTC ISO1601: " ++ fmt iso1601Zulu ut
+  putStrLn $ " UTC ISO1601: " <> fmt iso1601Zulu ut
 
-  putStrLn $ "\n   Unix time: " ++ fmt "%s" ut
-  putStrLn $ "milliseconds: " ++ (show . fromIntegral . truncate
-    . (* 1000) . realToFrac . utcTimeToPOSIXSeconds $ ut)
+  putStrLn $ "\n   Unix time: " <> fmt "%s" ut
+  putStrLn $ "milliseconds: " <>
+    (show . round . (* 1000) .  utcTimeToPOSIXSeconds $ ut)
 
   exitSuccess
 
